2bcd9cdd31a21b3e74f956612861085738e19ede
Adams Bala <adams.alok@gmail.com>
2023-11-16 00:11:06 -0500

    Project Init

---
+++ gui.py
@@ -0,0 +1,87 @@
+from ahk import AHK
+# import subprocess
+import time
+
+ahk = AHK()
+
+
+def find_window(title, run_cmd):
+    # Check if the Windows Terminal is running
+    win = ahk.find_window(title=title)
+    if win is None:
+        # If Windows Terminal is not running, start it
+        print(run_cmd)
+        ahk.run_script(run_cmd)  # Open windows terminal
+        print(run_cmd)
+        time.sleep(1)
+
+    # Bring Windows Terminal to the foreground
+    win = ahk.find_window(title=title)
+    return win
+
+
+def process_user_input(command):
+    win = find_window('ahk_exe WindowsTerminal.exe', 'Run wt')
+    if win:
+        win.activate()
+        ahk.send_input(f'{command}\n')
+
+
+def input_gui():
+    user_input = ahk.input_box(prompt='Command:', title='Enter Command')
+    if user_input:
+        if user_input in command_dictionary:
+            print(f"Starting Process: {user_input}")
+            command_dictionary[user_input]()
+        else:
+            process_user_input(user_input)
+
+
+def hotkey_triggered():
+    input_gui()
+
+
+def start_notepad():
+    win = find_window('ahk_exe notepad++.exe', 'Run notepad++')
+    if win:
+        win.activate()
+        ahk.send_input('^n')
+
+
+def vagrant_up():
+    win = find_window('ahk_exe WindowsTerminal.exe', 'Run wt')
+    if win:
+        win.activate()
+        ahk.send_input('cd Z:\\VirtualBox\\\\Vagrant \n')
+        ahk.send_input('vagrant up\n')
+
+
+def vagrant_ssh():
+    win = find_window('ahk_exe WindowsTerminal.exe', 'Run wt')
+    if win:
+        win.activate()
+        ahk.send_input('cd Z:\\VirtualBox\\\\Vagrant \n')
+        ahk.send_input('ssh vagrant@localhost -p 17122\n')
+        time.sleep(10)
+        ahk.send_input('vagrant\n')
+
+
+def genshin_impact():
+    win = find_window('ahk_exe launcher.exe', 'Run C:\\Program Files\\Genshin Impact\\launcher.exe')
+    if win:
+        win.activate()
+
+
+# ALT + Z
+ahk.add_hotkey('!z', callback=hotkey_triggered)
+
+command_dictionary = {
+    "notepad": start_notepad,
+    "vagrantup": vagrant_up,
+    "vagrantssh": vagrant_ssh,
+    "genshin": genshin_impact
+}
+
+# start the hotkey process thread
+ahk.start_hotkeys()
+ahk.block_forever()
